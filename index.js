419a3ebaeedeb9c11f6467056522569096256c5c
unknown <atikrahad1@gmail.com>
2024-03-14 09:13:58 +0600

    adding mongodb in server

--- index.js
+++ index.js
@@ -1,11 +1,44 @@
-const express = require("express")
-const app = express()
-const port = 5000
+const express = require("express");
+const app = express();
+require("dotenv").config();
+const port = 5000;
 
-app.get("/", (req, res)=>{
-    res.send("server is running")
-})
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const uri =
+  `mongodb+srv://${process.env.DB_user}:${process.env.DB_Pass}@cluster0.1lk0tsy.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
-app.listen(port, ()=>{
-    console.log("server is running with", port);
-})
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    
+    await client.connect();
+    
+
+    
+
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("server is running");
+});
+
+app.listen(port, () => {
+  console.log("server is running with", port);
+});
