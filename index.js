caaeb5c4b6f66bd4dfad7c8f58349ed4a6634378
unknown <atikrahad1@gmail.com>
2024-03-16 10:07:53 +0600

    adding note post api

--- index.js
+++ index.js
@@ -1,16 +1,14 @@
 const express = require("express");
 const app = express();
-const cors = require("cors")
+const cors = require("cors");
 require("dotenv").config();
 const port = 5000;
 
-app.use(cors())
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
 
 const { MongoClient, ServerApiVersion } = require("mongodb");
-const uri =
-  `mongodb+srv://${process.env.DB_user}:${process.env.DB_Pass}@cluster0.1lk0tsy.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
-
+const uri = `mongodb+srv://${process.env.DB_user}:${process.env.DB_Pass}@cluster0.1lk0tsy.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 const client = new MongoClient(uri, {
   serverApi: {
@@ -22,36 +20,41 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    
     await client.connect();
-    
 
-    const userCollection = client.db("MathHero").collection("users")
-    const nodlistCollection = client.db("MathHero").collection("nodelist")
+    const userCollection = client.db("MathHero").collection("users");
+    const nodlistCollection = client.db("MathHero").collection("nodelist");
+    const noteCollection = client.db("MathHero").collection("notes");
 
-    app.get("/user", async(req, res)=>{
-        const info = req.query.email
-        const result = await userCollection.findOne({email: info})
-        res.send(result)
-    })
+    app.get("/user", async (req, res) => {
+      const info = req.query.email;
+      const result = await userCollection.findOne({ email: info });
+      res.send(result);
+    });
 
-    app.post("/user", async(req, res)=>{
-        const info = req.body
-        const result =await userCollection.insertOne(info)
-        res.send(result)
-    })
+    app.get("/notelist", async (req, res) => {
+      const query = req.query.email;
+      const result = await nodlistCollection.find({ email: query }).toArray();
+      res.send(result);
+    });
 
-    app.get("/notelist", async(req, res)=>{
-      const query = req.query.email
-      const result = await nodlistCollection.find({email: query}).toArray()
-      res.send(result)
-    })
+    app.post("/user", async (req, res) => {
+      const info = req.body;
+      const result = await userCollection.insertOne(info);
+      res.send(result);
+    });
 
-    app.post("/notelist", async(req, res)=>{
-      const info = req.body
-      const result = await nodlistCollection.insertOne(info)
-      res.send(result)
-    })
+    app.post("/notelist", async (req, res) => {
+      const info = req.body;
+      const result = await nodlistCollection.insertOne(info);
+      res.send(result);
+    });
+
+    app.post("/note", async (req, res) => {
+      const info = req.body;
+      const result = await noteCollection.insertOne(info);
+      res.send(result);
+    });
 
     await client.db("admin").command({ ping: 1 });
     console.log(
